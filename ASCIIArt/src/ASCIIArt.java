bf41fea801cea78b77f2e87e541aa6fe8b587a10
benki <benki@BENKIM>
2019-09-20 13:24:53 -0700

    turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -16,12 +16,104 @@
 
 // YOU CODE HERE :) 
 
+
+// MONA LISA
 public class ASCIIArt {
 	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+		text();
+		//line 1
+		System.out.print("\n \t  ");
+		l();
+		//line 2
+		System.out.print("\n\t");
+		eightPrinter(7);
+		//line 3
+		System.out.print("\n      ");
+		eightPrinter(11);
+		//line 4
+		System.out.print("\n     ");
+		eightPrinter(1);
+		System.out.print("        ");
+		eightPrinter(3);
+		//line 5
+		System.out.print("\n    ");
+		eightPrinter(1);
+		System.out.print("\t\t");
+		eightPrinter(3);
+		//line 6
+		System.out.print("\n   ");
+		eightPrinter(1);
+		System.out.print(".-=. ,==-. ");
+		eightPrinter(3);
+		//line 7
+		System.out.print("\n   ");
+		eightPrinter(1);
+		System.out.print(" `~`  `~'  ");
+		eightPrinter(3);
+		//line 8
+		System.out.print("\n   ");
+		eightPrinter(1);
+		System.out.print("\t\t");
+		eightPrinter(3);
+		//line 9
+		System.out.print("\n   ");
+		eightPrinter(1);
+		System.out.print("8  `-~  ' ");
+		c();
+		eightPrinter(3);
+		//line 10
+		System.out.print("\n   ");
+		eightPrinter(3);
+		System.out.print(" ~==~ . ");
+		c();
+		eightPrinter(3);
+		//line 11
+		System.out.print("\n   ");
+		eightPrinter(17);
+		//line 12 
+		System.out.print("\n   ");
+		eightPrinter(17);
+		//line 13
+		System.out.print("\n  8");
+		eightPrinter(19);
+		//line 14
+		System.out.print("\n");
+		l();
+		l();
+		l();
+		l();
+		l();
+		l();
+		
+		
 	}
+
+	public static void l() {
+		//lines
+		System.out.print("____");
+	}
+	public static void eightPrinter(int numberOfEights) {
+		//eight
+		while(numberOfEights >= 0) {
+			System.out.print("8");
+			numberOfEights -= 1;
+		}
+		
+	}
+	public static void c() {
+		//colon
+		System.out.print(":");
+	}
+	public static void text() {
+		//text from http://patorjk.com/software/taag/#p=display&f=Avatar&t=Mona%20Lisa%20
+		
+		System.out.println(" _      ____  _      ____    _     _  ____  ____   \r\n" + 
+				"/ \\__/|/  _ \\/ \\  /|/  _ \\  / \\   / \\/ ___\\/  _ \\  \r\n" + 
+				"| |\\/||| / \\|| |\\ ||| / \\|  | |   | ||    \\| / \\|  \r\n" + 
+				"| |  ||| \\_/|| | \\||| |-||  | |_/\\| |\\___ || |-||  \r\n" + 
+				"\\_/  \\|\\____/\\_/  \\|\\_/ \\|  \\____/\\_/\\____/\\_/ \\|  \r\n" + 
+				"                                                   ");
+
+	}	
+
 }
